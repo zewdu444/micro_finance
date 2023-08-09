13b24f7ba668d8028c3d60ebb68915348415fa55
zewdu erkyhun <zewdu444@gmail.com>
2023-08-09 21:22:03 +0300

    Add email send functionalities

--- server/api/endpoints/passwordmgt.py
+++ server/api/endpoints/passwordmgt.py
@@ -7,8 +7,35 @@ from sqlalchemy.orm import Session
 from .auth  import get_password_hash
 import datetime
 from .auth import create_access_token
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
 router = APIRouter(tags=["password management"], responses={404: {"description": "Not found"}})
 models.Base.metadata.create_all(bind=engine)
+
+conf = ConnectionConfig(
+    MAIL_USERNAME = "zewdu.anley@email.com",
+    MAIL_PASSWORD = "0013B5526C23A34E9E3107BD101D1B6DBF0D",
+    MAIL_FROM = "zewdu444@gmail.com",
+    MAIL_PORT = 2525,
+    MAIL_SERVER = "smtp.elasticemail.com",
+    MAIL_FROM_NAME="Zewdu Anley",
+    MAIL_STARTTLS = True,
+    MAIL_SSL_TLS = False,
+    USE_CREDENTIALS = True,
+    VALIDATE_CERTS = True,
+
+)
+
+@router.post("/send_email")
+async  def simple_send():
+    html ="""<p>Hi this test mail, thanks for using Fastapi-mail</p> """
+    message = MessageSchema(
+        subject="Fastapi-Mail module",
+        recipients= ["arrow9941519zewdu@gmail.com","zewdu.erkyhun@yandex.com"],
+        body=html,
+        subtype=MessageType.html)
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    return {"message": "sent"}
 
 @router.put("/resetpassword")
 async def reset_password(user: schemas.UserResetPassword, db: Session = Depends(get_db)):
