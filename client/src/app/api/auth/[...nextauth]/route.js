bb19e85ab717bee112296b0a33062c28ea1cf272
zewdu erkyhun <zewdu444@gmail.com>
2023-09-03 13:22:09 +0300

    Add login and register page

--- client/src/app/api/auth/[...nextauth]/route.js
+++ client/src/app/api/auth/[...nextauth]/route.js
@@ -24,36 +24,48 @@ const handler= NextAuth({
                  "username": credentials?.username,
                  "password": credentials?.password
               })
-           }).catch((error) => {
-              console.log(error)
            })
-            const user = res.data()
-            if (user) {
-                redirect("/")
-              // Any object returned will be saved in `user` property of the JWT
-              return user
-            } else {
-               console.log("check your details")
-              // If you return null then an error will be displayed advising the user to check their details.
-              return null
-
-              // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+           const user = await res.json();
+           if (!res.ok) {
+            throw new Error(user.message);
             }
+
+            if (res.ok && user) {
+
+              return user
+            }
+            return null
           }
         })
       ],
+      secret: process.env.NEXTAUTH_SECRET,
       session: {
          strategy : "jwt",
       },
+      pages: {
+        signIn: 'auth/login',
+      },
       callbacks: {
-          async jwt(token, user, account, profile, isNewUser) {
-            // Add access_token to the token right after signin
-            if (user) {
-               token.accessToken = user.accessToken
-            }
-            return token
+        async jwt({ token, user, account }) {
+          if (account && user) {
+            return {
+              ...token,
+              accessToken: user.token,
+              refreshToken: user.refreshToken,
+            };
           }
-      }
+
+          return token;
+        },
+        },
+        async session({ session, token }) {
+          session.user.accessToken = token.accessToken;
+          session.user.refreshToken = token.refreshToken;
+          session.user.accessTokenExpires = token.accessTokenExpires;
+
+          return session;
+        },
+      debug: process.env.NODE_ENV === 'development',
 
 })
 
